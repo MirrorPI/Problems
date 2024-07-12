ed184944a6b1825dd0c84123f3f88f5c70221de1
soos <92793904+MirrorPI@users.noreply.github.com>
2024-07-12 19:30:56 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+    while(1){
+        if(scanf("%d %d", &a, &b) != 2){
+            return 0;
+        }
+        printf("%d\n", a+b);
+    }
+
+    return 0;
+}
