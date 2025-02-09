56181a3096449156e925b8892f4cf2461ffdec23
soos <92793904+MirrorPI@users.noreply.github.com>
2025-02-10 01:22:19 +0900

    [D1] Title: 대각선 출력하기, Time: 93 ms, Memory: 25,600 KB -BaekjoonHub

---
+++ SWEA/D1/2027. 대각선 출력하기/대각선 출력하기.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Solution {
+
+	public static void main(String[] args) throws IOException{
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		for(int i=0; i<5; i++) {
+			for(int j=0; j<5; j++) {
+				if(i == j) {
+					bw.write("#");
+				}
+				else {
+					bw.write("+");
+				}
+				
+			}
+			bw.write("\n");
+		}
+		
+		bw.flush();
+		bw.close();
+
+	}
+
+}
