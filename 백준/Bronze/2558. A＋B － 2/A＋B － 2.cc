c29b11f8e62893653efa04df9a8631e6c14438a7
soos <92793904+MirrorPI@users.noreply.github.com>
2025-06-28 17:00:34 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int A, B, result;
+    cin>>A>>B;
+    result = A + B;
+
+    cout<<result;
+    return 0;
+}
