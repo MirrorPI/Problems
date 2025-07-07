9286678d25bd6babf988fc38c4d73191ac19051c
soos <92793904+MirrorPI@users.noreply.github.com>
2025-07-07 20:40:00 +0900

    [Bronze V] Title: 등록, Time: 92 ms, Memory: 13868 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        StringBuilder sb = new StringBuilder();
+        sb.append(214).append("\n").append("moneycant");
+        System.out.print(sb);
+    }
+}
