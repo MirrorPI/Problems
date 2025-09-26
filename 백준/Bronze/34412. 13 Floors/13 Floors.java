8b66ba5dd68269b22783b022fca53c3262a73f28
soos <92793904+MirrorPI@users.noreply.github.com>
2025-09-26 23:20:52 +0900

    [Bronze V] Title: 13 Floors, Time: 100 ms, Memory: 14044 KB -BaekjoonHub

---
+++ 백준/Bronze/34412. 13 Floors/13 Floors.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        if (N > 12){
+            N += 1;
+        }
+        System.out.print(N);
+    }
+
+}
