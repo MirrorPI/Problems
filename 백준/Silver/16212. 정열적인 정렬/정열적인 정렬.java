eaee16097cbf757f5de006e21f63773fc7bf20b6
soos <92793904+MirrorPI@users.noreply.github.com>
2025-11-25 00:56:56 +0900

    [Silver V] Title: 정열적인 정렬, Time: 968 ms, Memory: 112816 KB, Score: 25 point -BaekjoonHub

---
+++ 백준/Silver/16212. 정열적인 정렬/정열적인 정렬.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        StringBuilder sb = new StringBuilder();
+        for(int n : arr){
+            sb.append(n).append(" ");
+        }
+        System.out.print(sb);
+    }
+}
