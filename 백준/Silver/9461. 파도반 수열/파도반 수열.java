df2371758e03f9b847fdd3484920595d422c504f
soos <92793904+MirrorPI@users.noreply.github.com>
2025-12-20 01:13:09 +0900

    [Silver III] Title: 파도반 수열, Time: 64 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static long[] dp = new long[101];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 1;
+        dp[4] = 2;
+        dp[5] = 2;
+        for(int i=6; i<=100; i++){
+            dp[i] = dp[i-5] + dp[i-1];
+        }
+        int T = Integer.parseInt(br.readLine());
+        for(int t=1; t<=T; t++){
+            int N = Integer.parseInt(br.readLine());
+            sb.append(dp[N]).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
