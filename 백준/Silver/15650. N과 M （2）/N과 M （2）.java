a0d01c88abc33e4d3ac8cf15d0a895ee89816b3b
soos <92793904+MirrorPI@users.noreply.github.com>
2026-01-10 17:30:48 +0900

    [Silver III] Title: N과 M (2), Time: 60 ms, Memory: 11520 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,39 +1,36 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	static int N, M;
-	static int[] result;
-	static StringBuilder sb = new StringBuilder();
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		result = new int[M+1];
-		solve(1, 0);
-		System.out.print(sb);
-		br.close();
-	}
-	
-	private static void solve(int s, int cnt) {
-		if(cnt == M) {
-			for(int i=0; i<M; i++) {
-				sb.append(result[i]).append(" ");
-			}
-			sb.append("\n");
-			return;
-		}
-		
-		for(int i=s; i<=N; i++) {
-			
-			result[cnt] = i;
-			solve(i+1 , cnt+1);
-		}
-	}	
+    static int N, M; //1~8;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[M];
+        combination(0, 1);
+
+        System.out.print(sb);
+    }
+
+    public static void combination(int depth, int start) {
+        if(depth == M) {
+            for(int num : arr) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        if(start > N) return;
+
+        arr[depth] = start;
+        combination(depth + 1, start + 1);
+
+        combination(depth, start+1);
+    }
 }
