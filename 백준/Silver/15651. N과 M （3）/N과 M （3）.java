a7a497b77c4e27d089560a7ed4cbec6fad03e680
soos <92793904+MirrorPI@users.noreply.github.com>
2026-01-11 23:54:44 +0900

    [Silver III] Title: N과 M (3), Time: 332 ms, Memory: 112056 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,36 +1,33 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringBuilder sb = new StringBuilder(); 
-	static int N, M; //1~7
-	static int[] result;
-	
-	public static void main(String[] args) throws IOException {
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		result = new int[M];
-		
-		solve(0);
-		System.out.print(sb);
-		br.close();
-	}
-	public static void solve(int cnt) {
-		if(cnt == M) {
-			for(int n : result) {
-				sb.append(n).append(" ");
-			}
-			sb.append("\n");
-			return;
-		}
-		for(int i=1; i<=N; i++) {
-			result[cnt] = i;
-			solve(cnt+1);
-		}
-	}
+    static int N, M;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[M];
+        solve(0, 1);
+        System.out.print(sb);
+    }
+    public static void solve(int depth, int start) {
+        if(depth == M) {
+            for(int num : arr){
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=1; i<=N; i++){
+            arr[depth] = i;
+            solve(depth+1, i);
+        }
+    }
 }
