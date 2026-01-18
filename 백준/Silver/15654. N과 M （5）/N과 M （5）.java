7f79b66407d143926a9013dead00a56b17aca2cd
soos <92793904+MirrorPI@users.noreply.github.com>
2026-01-19 00:47:51 +0900

    [Silver III] Title: N과 M (5), Time: 228 ms, Memory: 31312 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N, M;
+    static int[] arr;
+    static int[] ans;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        ans = new int[M];
+        visited = new boolean[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        solve(0);
+        System.out.print(sb);
+
+    }
+    public static void solve(int depth) {
+        if(depth == M) {
+            for(int num : ans){
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=0; i<N; i++){
+            if(visited[i]) continue;
+
+            ans[depth] = arr[i];
+            visited[i] = true;
+            solve(depth+1);
+            visited[i] = false;
+        }
+    }
+}
