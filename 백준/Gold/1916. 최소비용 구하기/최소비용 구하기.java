470f493a65fa4d7cc8bd74892ec358062a967110
soos <92793904+MirrorPI@users.noreply.github.com>
2026-03-21 22:54:18 +0900

    [Gold V] Title: 최소비용 구하기, Time: 532 ms, Memory: 56052 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,53 +1,79 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
+    public static void main(String[] args) throws Exception {
+        input();
+        solve();
+    }
 
     static int N; //도시의 개수(1~1000)
     static int M; //버스의 개수(1~100,000)
-    static List<int[]>[] graph;
-    static final int INF = Integer.MAX_VALUE;
-    static int[] d;
+    static ArrayList<Bus>[] buses;
+    static int[] minTotalDist;
+    static class City {
+        int x; //현재 위치
+        int totalDist; //현재까지 비용
+        City (int x, int totalDist) {
+            this.x = x;
+            this.totalDist = totalDist;
+        }
+    }
+    static class Bus {
+        int to; //도착 지점
+        int dist; //버스 비용
+        Bus (int to, int dist) {
+            this.to = to;
+            this.dist = dist;
+        }
+    }
+    static int start;
+    static int end;
+    final static int INF = 100_000_000;
 
-    public static void main(String[] args) throws IOException{
+    public static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
         N = Integer.parseInt(br.readLine());
         M = Integer.parseInt(br.readLine());
-        graph = new ArrayList[N+1];
-        for(int i=1; i<=N; i++){
-            graph[i] = new ArrayList<>();
+        buses = new ArrayList[N+1];
+        minTotalDist = new int[N+1];
+        Arrays.fill(minTotalDist, INF);
+        for(int i=1; i<=N; i++) {
+            buses[i] = new ArrayList<>();
         }
-        d = new int[N+1];
-        Arrays.fill(d, INF);
         for(int i=0; i<M; i++){
-            st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-            graph[u].add(new int[] {v, w});
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken());
+            buses[s].add(new Bus(e, d));
         }
-        st = new StringTokenizer(br.readLine());
-        int s = Integer.parseInt(st.nextToken());
-        int e = Integer.parseInt(st.nextToken());
-        PriorityQueue<int[]> pq = new PriorityQueue<>((n1, n2) -> {
-            return Integer.compare(n1[1], n2[1]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        start = Integer.parseInt(st.nextToken());
+        end = Integer.parseInt(st.nextToken());
+    }
+
+    public static void solve() {
+        PriorityQueue<City> pq = new PriorityQueue<>((c1, c2) -> {
+            return Integer.compare(c1.totalDist, c2.totalDist);
         });
-        d[s] = 0;
-        pq.offer(new int[] {s, d[s]});
+        pq.add(new City(start, 0));
+        minTotalDist[start] = 0;
         while(!pq.isEmpty()){
-            int[] cur = pq.poll();
-            if(d[cur[0]] < cur[1]) continue;
+            City curr = pq.poll();
 
-            for(int[] nxt : graph[cur[0]]){
-                if(d[nxt[0]] <= d[cur[0]] + nxt[1]) continue;
+            if(curr.totalDist > minTotalDist[curr.x]) continue;
 
-                d[nxt[0]] = d[cur[0]] + nxt[1];
-                pq.offer(new int[] {nxt[0], d[nxt[0]]});
+            for(Bus next : buses[curr.x]) {
+                int nextTotalDist = curr.totalDist + next.dist;
+
+                if(minTotalDist[next.to] <= nextTotalDist) continue;
+
+                pq.add(new City(next.to, nextTotalDist));
+                minTotalDist[next.to] = nextTotalDist;
             }
         }
-        System.out.print(d[e]);
-        br.close();
+
+        System.out.print(minTotalDist[end]);
     }
 }
