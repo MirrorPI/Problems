96ad43436c6df0e1be59a71ae512896807d06419
soos <92793904+MirrorPI@users.noreply.github.com>
2026-04-21 19:05:07 +0900

    [level 3] Title: 단어 변환, Time: 0.24 ms, Memory: 69.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/README.md
+++ 프로그래머스/3/43163. 단어 변환/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단어 변환 - 43163 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163?language=java) 
 
 ### 성능 요약
 
-메모리: 69.7 MB, 시간: 0.98 ms
+메모리: 69.4 MB, 시간: 0.24 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 20일 02:49:03
+2026년 04월 21일 19:04:57
 
 ### 문제 설명
 
--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -3,102 +3,62 @@ import java.util.*;
 class Solution {
 
     public int solution(String begin, String target, String[] words) {
-        if (!containsTarget(target, words)) {
+        if(!containsTarget(target, words)) {
             return 0;
         }
-
-        WordGraph graph = new WordGraph(begin, words);
-        return bfs(begin, target, graph);
+        
+        return bfs(begin, target, words);
     }
 
-    private int bfs(String begin, String target, WordGraph graph) {
-        Queue<WordState> queue = new ArrayDeque<>();
-        Set<String> visited = new HashSet<>();
-
-        queue.offer(new WordState(begin, 0));
-        visited.add(begin);
-
-        while (!queue.isEmpty()) {
-            WordState current = queue.poll();
-
-            if (current.word.equals(target)) {
-                return current.count;
-            }
-
-            for (String nextWord : graph.getNextWords(current.word)) {
-                if (visited.contains(nextWord)) {
-                    continue;
+    public int bfs(String begin, String target, String[] words) {
+        Queue<String> q = new ArrayDeque<>();
+        boolean[] visited = new boolean[words.length];
+        
+        q.add(begin);
+        int count = 0;
+        while(!q.isEmpty()) {
+            int size = q.size();
+            
+            for(int i=0; i<size; i++) {
+                String curr = q.poll();
+                
+                if(curr.equals(target)) {
+                    return count;
                 }
-
-                visited.add(nextWord);
-                queue.offer(new WordState(nextWord, current.count + 1));
+                
+                for(int j=0; j<words.length; j++) {
+                    if(visited[j]) continue;
+                    
+                    if(canTransform(curr, words[j])) {
+                        q.add(words[j]);
+                        visited[j] = true;
+                    }
+                }
             }
+            count++;
         }
-
-        return 0;
+        return count;
     }
-
-    private boolean containsTarget(String target, String[] words) {
-        for (String word : words) {
-            if (word.equals(target)) {
+    
+    public boolean containsTarget(String target, String[] words) {
+        for(String word : words) {
+            if(word.equals(target)) {
                 return true;
             }
         }
         return false;
     }
-
-    private static final class WordState {
-        private final String word;
-        private final int count;
-
-        private WordState(String word, int count) {
-            this.word = word;
-            this.count = count;
-        }
-    }
-
-    private static final class WordGraph {
-        private final Map<String, List<String>> adjacency = new HashMap<>();
-
-        private WordGraph(String begin, String[] words) {
-            List<String> allWords = new ArrayList<>();
-            allWords.add(begin);
-            Collections.addAll(allWords, words);
-
-            for (String word : allWords) {
-                adjacency.put(word, new ArrayList<>());
-            }
-
-            for (int i = 0; i < allWords.size(); i++) {
-                for (int j = i + 1; j < allWords.size(); j++) {
-                    String word1 = allWords.get(i);
-                    String word2 = allWords.get(j);
-
-                    if (canTransform(word1, word2)) {
-                        adjacency.get(word1).add(word2);
-                        adjacency.get(word2).add(word1);
-                    }
+    public boolean canTransform (String u, String v) {
+        int diffCount = 0;
+        
+        for(int i=0; i<u.length(); i++){
+            if(u.charAt(i) != v.charAt(i)) {
+                diffCount++;
+                if(diffCount > 1) {
+                    return false;
                 }
             }
         }
-
-        private List<String> getNextWords(String word) {
-            return adjacency.getOrDefault(word, Collections.emptyList());
-        }
-
-        private boolean canTransform(String word1, String word2) {
-            int diffCount = 0;
-
-            for (int i = 0; i < word1.length(); i++) {
-                if (word1.charAt(i) != word2.charAt(i)) {
-                    diffCount++;
-                    if (diffCount > 1) {
-                        return false;
-                    }
-                }
-            }
-
-            return diffCount == 1;
-        }
+        return diffCount == 1;
     }
 }
