45881853786156ae74659f2c0b82541fd41a5a5c
soos <92793904+MirrorPI@users.noreply.github.com>
2026-04-26 21:40:59 +0900

    [level 3] Title: 여행경로, Time: 0.79 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,53 +1,50 @@
 import java.util.*;
 
 class Solution {
-    
-    String[][] sortedTickets;
+    String[][] tickets;
     boolean[] used;
     String[] answer;
     int n;
-    
-    
+
     public String[] solution(String[][] tickets) {
-        //만일 가능한 경로가 2개 이상일 경우 알파벳 순서가 앞서는 경로를 return 합니다.
-        //알파벳순 정렬 -> 알파벳순으로 가능여부를 확인
-        Arrays.sort(tickets, (t1, t2) -> {
-            if(t1[0].equals(t2[0])) {
-                return t1[1].compareTo(t2[1]);
+        this.tickets = tickets;
+        this.n = tickets.length;
+        this.used = new boolean[n];
+
+        Arrays.sort(this.tickets, (a, b) -> {
+            if (a[0].equals(b[0])) {
+                return a[1].compareTo(b[1]);
             }
-            return t1[0].compareTo(t2[1]);
+            return a[0].compareTo(b[0]);
         });
-        sortedTickets = tickets;
-        n = tickets.length;
-        used = new boolean[n];
-        
-        String[] path = new String[n+1];
-        String start = "ICN";
-        path[0] = start;
-        dfs(start, 0, path);
-        
+
+        String[] path = new String[n + 1];
+        path[0] = "ICN";
+
+        dfs("ICN", 0, path);
+
         return answer;
     }
-    
-    public boolean dfs(String curr, int depth, String[] path) {
-        if(depth == n) {
-            answer = path;
+
+    boolean dfs(String current, int depth, String[] path) {
+        if (depth == n) {
+            answer = path.clone();
             return true;
         }
-        
-        for(int i=0; i<n; i++) {
-            if(used[i] || !curr.equals(sortedTickets[i][0])) continue;
-               
-            used[i] = true;
-            path[depth+1] = sortedTickets[i][1];
-            
-            if(dfs(sortedTickets[i][1], depth+1, path)) {
-                return true;
+
+        for (int i = 0; i < n; i++) {
+            if (!used[i] && tickets[i][0].equals(current)) {
+                used[i] = true;
+                path[depth + 1] = tickets[i][1];
+
+                if (dfs(tickets[i][1], depth + 1, path)) {
+                    return true;
+                }
+
+                used[i] = false;
             }
-            
-            used[i] = false;
         }
-        
+
         return false;
     }
 }
