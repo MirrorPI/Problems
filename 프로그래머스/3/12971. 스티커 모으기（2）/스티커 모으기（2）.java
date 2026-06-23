3f4824bfb92cafb152513ca9da55d24f1118b8a9
soos <92793904+MirrorPI@users.noreply.github.com>
2026-06-23 23:41:10 +0900

    [level 3] Title: 스티커 모으기(2), Time: 4.80 ms, Memory: 61.5 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -1,33 +1,27 @@
 class Solution {
     public int solution(int sticker[]) {
-        int n = sticker.length;
-
-        if (n == 1) {
-            return sticker[0];
-        }
-
-        int firstChoice = getMax(sticker, 0, n - 2); 
-        int firstNotChoice = getMax(sticker, 1, n - 1);
-
+        int len = sticker.length;
+        
+        if(len == 1) return sticker[0];
+        
+        int firstChoice = calMax(sticker, 0, len-2);
+        int firstNotChoice = calMax(sticker, 1, len-1);
+            
         return Math.max(firstChoice, firstNotChoice);
     }
-
-    private int getMax(int[] sticker, int start, int end) {
-        int len = end - start + 1;
-
-        if (len == 1) {
-            return sticker[start];
-        }
-
+    
+    
+    public int calMax(int[] sticker, int s, int e) {
+        int len = e - s + 1;
+        if(len == 1) return sticker[s];
+            
         int[] dp = new int[sticker.length];
-
-        dp[start] = sticker[start];
-        dp[start + 1] = Math.max(sticker[start], sticker[start + 1]);
-
-        for (int i = start + 2; i <= end; i++) {
-            dp[i] = Math.max(dp[i - 1], dp[i - 2] + sticker[i]);
+        dp[s] = sticker[s];
+        dp[s+1] = Math.max(sticker[s], sticker[s+1]);
+        for(int i=s; i<e-1; i++) {
+            dp[i+2] = Math.max(dp[i] + sticker[i+2], dp[i+1]);
         }
-
-        return dp[end];
-    }
+        
+        return dp[e];
+     }
 }
