c3ae16f88103c5c308db3abc8ec3800d542147e2
soos <92793904+MirrorPI@users.noreply.github.com>
2026-07-05 23:55:00 +0900

    [level 3] Title: 카테고리 별 도서 판매량 집계하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/144855. 카테고리 별 도서 판매량 집계하기/카테고리 별 도서 판매량 집계하기.sql
+++ 프로그래머스/3/144855. 카테고리 별 도서 판매량 집계하기/카테고리 별 도서 판매량 집계하기.sql
@@ -4,4 +4,4 @@ inner join book_sales s
 on b.book_id = s.book_id
 where s.sales_date >= '2022-01-01' and s.sales_date < '2022-02-01'
 group by b.category
-order by b.category asc
+order by b.category
