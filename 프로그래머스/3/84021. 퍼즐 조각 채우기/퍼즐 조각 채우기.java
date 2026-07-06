cd5b6d011747cc39dd0f541489abb7c3e27049b3
soos <92793904+MirrorPI@users.noreply.github.com>
2026-07-06 18:48:33 +0900

    [level 3] Title: 퍼즐 조각 채우기, Time: 55.02 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/3/84021. 퍼즐 조각 채우기/퍼즐 조각 채우기.java
+++ 프로그래머스/3/84021. 퍼즐 조각 채우기/퍼즐 조각 채우기.java
@@ -10,20 +10,24 @@ class Solution {
         void add(Point point) {
             points.add(point);
         }
+
         int size() {
             return points.size();
         }
+
         void sort() {
             Collections.sort(points, (p1, p2) -> {
-            if(p1.x == p2.x) return p1.y - p2.y;
-            
-            return p1.x - p2.x;
+                if (p1.x == p2.x) return p1.y - p2.y;
+
+                return p1.x - p2.x;
             });
         }
     }
+
     class Point {
         int x;
         int y;
+
         Point(int x, int y) {
             this.x = x;
             this.y = y;
@@ -41,70 +45,77 @@ class Solution {
         int blockCnt = blocks.size();
         boolean[] usedBlock = new boolean[blockCnt];
 
-        for(Shape blank : blanks) {
-            blank.sort();
-            for(int i=0; i<blockCnt; i++) {
-                if(usedBlock[i]) continue;
-                
-                if(canFit(blocks.get(i), blank)) {
+        for (Shape blank : blanks) {
+            for (int i = 0; i < blockCnt; i++) {
+                if (usedBlock[i]) continue;
+
+                if (canFit(blocks.get(i), blank)) {
                     usedBlock[i] = true;
-                    
+
                     ans += blocks.get(i).size();
                     break;
                 }
             }
         }
-        
+
         return ans;
     }
-    
+
     public boolean canFit(Shape block, Shape blank) {
         Shape rotatedBlock = block;
-        
-        for(int i=0; i<4; i++) {
-             rotatedBlock.sort();
-            if(isSameShape(rotatedBlock, blank)) return true;
-            
-            rotatedBlock = nomalize(rotate90(rotatedBlock));
+
+        for (int i = 0; i < 4; i++) {
+            if (isSameShape(rotatedBlock, blank)) return true;
+
+            rotatedBlock = normalize(rotate90(rotatedBlock));
         }
-        
+
         return false;
     }
-    public Shape nomalize(Shape shape) {
-        Shape nomalizedShape = new Shape();
-        int minX = 55;
-        int minY = 55;
-        for(Point p : shape.points) {
+
+    public Shape normalize(Shape shape) {
+        Shape normalizedShape = new Shape();
+
+        int minX = Integer.MAX_VALUE;
+        int minY = Integer.MAX_VALUE;
+
+        for (Point p : shape.points) {
             int x = p.x;
             int y = p.y;
+
             minX = Math.min(x, minX);
             minY = Math.min(y, minY);
         }
-        for(Point p : shape.points) {
-            nomalizedShape.add(new Point(p.x - minX, p.y - minY));
+
+        for (Point p : shape.points) {
+            normalizedShape.add(new Point(p.x - minX, p.y - minY));
         }
-        return nomalizedShape;
+
+        normalizedShape.sort();
+
+        return normalizedShape;
     }
 
-    
     public boolean isSameShape(Shape block, Shape blank) {
-        if(block.size() != blank.size()) return false;
+        if (block.size() != blank.size()) return false;
 
-        for(int i=0; i<block.size(); i++) {
+        for (int i = 0; i < block.size(); i++) {
             Point p1 = block.points.get(i);
             Point p2 = blank.points.get(i);
-            
-            if(p1.x != p2.x || p1.y != p2.y) return false;
+
+            if (p1.x != p2.x || p1.y != p2.y) return false;
         }
-        
+
         return true;
     }
+
     public Shape rotate90(Shape shape) {
         Shape rotatedShape = new Shape();
-        for(Point p : shape.points) {
+
+        for (Point p : shape.points) {
             rotatedShape.add(new Point(p.y, p.x * -1));
         }
-        
+
         return rotatedShape;
     }
 
@@ -112,45 +123,46 @@ class Solution {
         int xLen = map.length;
         int yLen = map[0].length;
         boolean[][] visited = new boolean[xLen][yLen];
-        for(int x=0; x<xLen; x++) {
-            for(int y=0; y<yLen; y++) {
-                if(visited[x][y] || map[x][y] != target) continue;
-                
-                shapes.add(findShapeByBFS(x, y, map, visited, target));
+
+        for (int x = 0; x < xLen; x++) {
+            for (int y = 0; y < yLen; y++) {
+                if (visited[x][y] || map[x][y] != target) continue;
+
+                Shape shape = findShapeByBFS(x, y, map, visited, target);
+                shapes.add(normalize(shape));
             }
         }
     }
-    
+
     public Shape findShapeByBFS(int x, int y, int[][] map, boolean[][] visited, int target) {
         Shape shape = new Shape();
-        
+
         Point start = new Point(x, y);
         shape.add(start);
-        
+
         Queue<Point> q = new ArrayDeque<>();
         q.add(start);
         visited[x][y] = true;
-        
-        while(!q.isEmpty()) {
+
+        while (!q.isEmpty()) {
             Point curr = q.poll();
             int cx = curr.x;
             int cy = curr.y;
-            
-            for(int i=0; i<4; i++) {
+
+            for (int i = 0; i < 4; i++) {
                 int nx = cx + dx[i];
                 int ny = cy + dy[i];
-                
-                if(nx < 0 || nx >= map.length || ny < 0 || ny >= map[0].length || visited[nx][ny]) continue;
-                
-                if(map[nx][ny] != target) continue;
-                
+
+                if (nx < 0 || nx >= map.length || ny < 0 || ny >= map[0].length || visited[nx][ny]) continue;
+
+                if (map[nx][ny] != target) continue;
+
                 shape.add(new Point(nx, ny));
                 visited[nx][ny] = true;
                 q.add(new Point(nx, ny));
             }
         }
 
-        
-        return nomalize(shape);
+        return shape;
     }
 }
