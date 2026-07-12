ccaf2bdd912d61d71955da1265c11f32edcce2b2
soos <92793904+MirrorPI@users.noreply.github.com>
2026-07-12 14:12:24 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 0.04 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String myString) {
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<myString.length(); i++) {
+            sb.append(Character.toLowerCase(myString.charAt(i)));
+        }
+        
+        return sb.toString();
+    }
+}
